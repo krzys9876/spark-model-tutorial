d87f44657d83613cebe52a6918eab90267acb3ae
Krzysztof Ruta <krzysztof.ruta@play.pl>
2022-06-20 11:17:09 +0200

    add mode data and update readme

--- sqlite/README.md
+++ sqlite/README.md
@@ -1,1 +1,30 @@
-TODO: describe SQLite prototype
+# Linear reggression model #
+### SQLite3 ###
+
+This is a simple application that processes information from temperature sensors 
+and calculates next expected temperature using linear regression.
+
+The linear regression formula is complicated enough so that it is necessary to write 
+a nested query. This is to demonstrate that SQL is not really capable of handling such formulas - 
+the overhead related to subsequent queries (nested or chained as view) makes formulas difficult to read.
+
+##Database structure##
+
+The application consists of two tables:
+1. **base** which contains full period data for a sensor in a given period, together with intermediate factors required to calculate linear regression
+2. **input** which contains new data for a current period
+
+The data flow is made of the following stages:
+
+1. determine current period from input data (assuming that this is constant for the whole table): **input_period** view
+2. get previous period base: **prev_base** view 
+3. join previous period base with current input: **prev_base_with_input** view
+4. calculate current period values: **prev_base_with_input_updated** view
+5. insert results into base and clear the input table: **lin_reg_process_input.sql** script
+
+The script to blank create database is **lin_reg_create.sql**. You may want to process all periods at once using **lin_reg_process_all.sql**
+
+Windows users may find useful simple batch files for creating, processing and viewing the database: 
+**lin_reg_create_db.bat**, **lin_reg_process.bat**, **browse.bat**. 
+
+All you need to do is make sure **sqlite3.exe** (or other appropriate executable for other OS) is accessible from the script directory.
--- sqlite/lin_reg_process_all.sql
+++ sqlite/lin_reg_process_all.sql
@@ -1,22 +1,22 @@
-insert into input (sensor,period,temp) values (1,1,16.132);
+insert into input (sensor,period,temp) values (1,1,16.132),(2,1,10.325);
 .read lin_reg_process_input.sql
-insert into input (sensor,period,temp) values (1,2,18.362);
+insert into input (sensor,period,temp) values (1,2,18.362),(2,2,10.364);
 .read lin_reg_process_input.sql
-insert into input (sensor,period,temp) values (1,3,17.81);
+insert into input (sensor,period,temp) values (1,3,17.81),(2,3,11.153);
 .read lin_reg_process_input.sql
-insert into input (sensor,period,temp) values (1,4,20.787);
+insert into input (sensor,period,temp) values (1,4,20.787),(2,4,11.053);
 .read lin_reg_process_input.sql
-insert into input (sensor,period,temp) values (1,5,19.649);
+insert into input (sensor,period,temp) values (1,5,19.649),(2,5,11.542);
 .read lin_reg_process_input.sql
-insert into input (sensor,period,temp) values (1,6,20.217);
+insert into input (sensor,period,temp) values (1,6,20.217),(2,6,11.73);
 .read lin_reg_process_input.sql
-insert into input (sensor,period,temp) values (1,7,21.811);
+insert into input (sensor,period,temp) values (1,7,21.811),(2,7,11.974);
 .read lin_reg_process_input.sql
-insert into input (sensor,period,temp) values (1,8,20.43);
+insert into input (sensor,period,temp) values (1,8,20.43),(2,8,11.875);
 .read lin_reg_process_input.sql
-insert into input (sensor,period,temp) values (1,9,22.688);
+insert into input (sensor,period,temp) values (1,9,22.688),(2,9,12.15);
 .read lin_reg_process_input.sql
-insert into input (sensor,period,temp) values (1,10,21.421);
+insert into input (sensor,period,temp) values (1,10,21.421),(2,10,12.136);
 .read lin_reg_process_input.sql
-insert into input (sensor,period,temp) values (1,11,22.98);
+insert into input (sensor,period,temp) values (1,11,22.98),(2,11,12.164);
 .read lin_reg_process_input.sql
